6d34d30159740be1f838a66e6e4e9e792b286d5c
chendiyao <304533091@qq.com>
2026-06-14 22:41:30 +0800

    Add global exception handling and disable single-file compression to fix ARM VM crash

--- Windows/SibeliusReset/App.xaml.cs
+++ Windows/SibeliusReset/App.xaml.cs
@@ -7,4 +7,24 @@ namespace SibeliusReset;
 /// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        // Catch UI thread exceptions
+        this.DispatcherUnhandledException += (s, args) =>
+        {
+            MessageBox.Show($"UI Thread Exception:\n{args.Exception.Message}\n\n{args.Exception.StackTrace}", "崩溃日志", MessageBoxButton.OK, MessageBoxImage.Error);
+            args.Handled = true;
+        };
+
+        // Catch background thread exceptions
+        AppDomain.CurrentDomain.UnhandledException += (s, args) =>
+        {
+            if (args.ExceptionObject is Exception ex)
+            {
+                MessageBox.Show($"Background Thread Exception:\n{ex.Message}\n\n{ex.StackTrace}", "崩溃日志", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        };
+    }
 }
